861ae2d56de6f18467c915228f191028ce55bce2
Luca Piombino <luca.piombino@codecoil.com>
2025-03-31 00:06:18 +0200

    Bash/ps1 script improvements/fixes
    - Fix: Ensure bash EOL is `LF` and not `CRLF`
    - Fix Apply fixes reported by shellcheck
    - Check if target exists before creating a directory.
      This should not be necessary, but i had some random occourrence where
      the installation was failing because directories/mounts already exists  and
      this seems to have solved the issue.
    - Add some volume mapping to cache known build artifacts to speed up local builds (`build.ps1`)

--- deployment/wsl-distro-rm.sh
+++ deployment/wsl-distro-rm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-distro="${1:-$WSL_DISTRO_NAME}"
+# distro="${1:-$WSL_DISTRO_NAME}"
 pkill -f container-desktop-proxy
 readlink /usr/local/bin/docker-compose && unlink /usr/local/bin/docker-compose
 readlink /usr/local/bin/docker && unlink /usr/local/bin/docker
--- deployment/wsl-init.sh
+++ deployment/wsl-init.sh
@@ -3,13 +3,17 @@ mkdir -p /mnt/host/wsl/container-desktop/cli-tools
 mkdir -p /mnt/host/wsl/container-desktop/certs
 mkdir -p /mnt/host/wsl/container-desktop/proxy
 mkdir -p /mnt/host/wsl/container-desktop/distro
-mkdir -p /mnt/wsl
+if [ ! -d "/mnt/wsl" ]; then
+    mkdir -p /mnt/wsl
+fi
 mkdir -p /var/lib/docker
 mount --bind /usr/local/bin/cli-tools /mnt/host/wsl/container-desktop/cli-tools
 mount --bind /certs /mnt/host/wsl/container-desktop/certs
 mount --bind /proxy /mnt/host/wsl/container-desktop/proxy
 mount --bind /distro /mnt/host/wsl/container-desktop/distro
-mount --bind /mnt/host/wsl /mnt/wsl
+# IMPORTANT: this should be rbind (r => recursive)
+# so that it also share other 
+mount --rbind /mnt/host/wsl /mnt/wsl
 mount --bind /mnt/host/wsl/container-desktop-data/data /var/lib/docker
 mkdir -p /sys/fs/cgroup/systemd
 if ! mountpoint -q /sys/fs/cgroup/systemd; then
